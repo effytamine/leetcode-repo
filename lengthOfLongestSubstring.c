e39716f32c9636db0239ee2a1360cc9c611c7d8d
effytamine <seanvilleza40@gmail.com>
2025-02-25 12:59:20 +0800

    add problem 3

--- lengthOfLongestSubstring.c
+++ lengthOfLongestSubstring.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int lengthOfLongestSubstring(char* s) {
-    
+    char* start_ptr = s;
+    int cnt = 0, longestsubstring = 0;
+    int j[256];
+    memset(j, -1, 256 * sizeof(int));
+
+    while(*s != '\0') {
+        if (j[(int)*s] == -1){
+            j[(int) *s] = (int)(s - start_ptr);
+            cnt++;
+        } else {
+            start_ptr++;
+            s = start_ptr;
+            cnt = 0;
+            memset(j, -1, 256 * sizeof(int));
+            continue;
+        }
+        if (cnt > longestsubstring) longestsubstring = cnt;
+        s++;
+    }
+    return longestsubstring;
+ }
+
+ int main() {
+    char string[] = "pwwkew";
+    int result = lengthOfLongestSubstring(string);
+    printf("Length of Longest Substring: %d\n", result);
+    return 0;
 }
